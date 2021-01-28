d7cf320acc1c44d5b88b9028ed6f8e3430dab4e3
jnyjhow <jhonatan_yamane@hotmail.com>
2021-01-28 14:48:03 -0300

    alterações feitas conforme combinado em call do dia 27/01/2021.

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Receita
 
 
-admin.site.register(Receita)
+@admin.register(Receita)
+class ReceitaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'descricao')
+    list_filter = ['titulo']
+    search_fields = ['descricao']
--- receitas/urls.py
+++ receitas/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from receitas import views
 
-app_name='receitas'
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
+
+    path('receitas/', views.ReceitaListView.as_view(), name='list'),
+    path('receitas/<int:pk>', views.ReceitaDetailView.as_view(), name='detail'),
 ]
